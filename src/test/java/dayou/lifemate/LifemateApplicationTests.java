6961d7ebadc0fe1b8712bb4e317a969f46db2d53
JoHyunwoo <jhw3874@gmail.com>
2025-02-19 20:59:15 +0900

    initial commit

---
+++ src/test/java/dayou/lifemate/LifemateApplicationTests.java
@@ -0,0 +1,13 @@
+package dayou.lifemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
