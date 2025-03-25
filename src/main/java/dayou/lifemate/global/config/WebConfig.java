8d617a82d11c86a788e01b71121d8de321593e6c
JoHyunwoo <jhw3874@gmail.com>
2025-03-26 00:06:38 +0900

    매트릭 수집용 queryInspector, 로컬 터미널 확인용 inspector 작성

--- src/main/java/dayou/lifemate/global/config/WebConfig.java
+++ src/main/java/dayou/lifemate/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import dayou.lifemate.global.interceptor.LocalQueryCountInterceptor;
 import dayou.lifemate.global.interceptor.QueryCountInterceptor;
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +13,11 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
 	private final QueryCountInterceptor countInterceptor;
+	private final LocalQueryCountInterceptor localQueryCountInterceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(countInterceptor);
+		registry.addInterceptor(localQueryCountInterceptor);
 	}
 }
